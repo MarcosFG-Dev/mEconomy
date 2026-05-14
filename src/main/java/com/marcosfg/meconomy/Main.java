ede099d6715b36894849b26e2e71ba01d058d011
Marcos Ferreira <57382861+MarcosFG-Dev@users.noreply.github.com>
2026-05-14 05:50:43 -0300

    Register money menu in main plugin class

--- src/main/java/com/marcosfg/meconomy/Main.java
+++ src/main/java/com/marcosfg/meconomy/Main.java
@@ -1,12 +1,12 @@
 package com.marcosfg.meconomy;
 
 import com.marcosfg.meconomy.api.VaultHook;
-import com.marcosfg.meconomy.commands.MoneyCommand;
 import com.marcosfg.meconomy.commands.MagnataCommand;
+import com.marcosfg.meconomy.commands.MoneyCommand;
+import com.marcosfg.meconomy.gui.MoneyMenu;
 import com.marcosfg.meconomy.manager.EconomyManager;
 import com.marcosfg.meconomy.storage.StorageManager;
 import net.milkbowl.vault.economy.Economy;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -16,6 +16,7 @@ public class Main extends JavaPlugin {
     private StorageManager storageManager;
     private EconomyManager economyManager;
     private VaultHook vaultHook;
+    private MoneyMenu moneyMenu;
 
     @Override
     public void onEnable() {
@@ -24,13 +25,15 @@ public class Main extends JavaPlugin {
 
         storageManager = new StorageManager(this);
         economyManager = new EconomyManager(this);
+        moneyMenu = new MoneyMenu(this);
 
         if (getServer().getPluginManager().getPlugin("Vault") != null) {
             setupVault();
         }
 
-        getCommand("money").setExecutor(new MoneyCommand(this));
+        getCommand("money").setExecutor(new MoneyCommand(this, moneyMenu));
         getCommand("magnata").setExecutor(new MagnataCommand(this));
+        getServer().getPluginManager().registerEvents(moneyMenu, this);
 
         getLogger().info("mEconomy carregado com sucesso!");
     }
@@ -59,5 +62,9 @@ public class Main extends JavaPlugin {
 
     public EconomyManager getEconomyManager() {
         return economyManager;
+    }
+
+    public MoneyMenu getMoneyMenu() {
+        return moneyMenu;
     }
 }
